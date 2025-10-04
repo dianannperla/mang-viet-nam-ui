415b5489c05dcbd9c3dfa9f80c0df7a08f5477c7
dianannperla <waafirmelyza@hotmail.com>
2025-10-04 18:10:27 +0100

    Organize exports alphabetically for better readability

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
+export { default as Alert } from './components/Alert';
 export { default as Button } from './components/Button';
 export { default as Modal } from './components/Modal';
-export { default as Alert } from './components/Alert';
 export { default as Tooltip } from './components/Tooltip';
